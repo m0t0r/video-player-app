30e9c9c5f952e23066e087e004a2404cc3b08284
m0t0r <tkachenko.vitaly.job@gmail.com>
2016-02-20 15:13:59 -0500

    feat(component): Create App component

---
+++ src/app.component.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import template from './app.component.html';
+
+class AppComponentCtrl {
+
+  constructor() {
+
+  }
+
+}
+
+
+let AppComponent = {
+  controller: AppComponentCtrl,
+  controllerAs: 'vm',
+  template
+};
+
+export default AppComponent;
--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
-(function() {
-  'use strict';
+'use strict';
 
-  let test = "Hello";
-  alert(test);
+import angular from 'angular';
+import ngAnimate from 'angular-animate';
+import ngAria from 'angular-aria';
+import ngMessages from 'angular-messages';
+import ngMaterial from 'angular-material';
+import uiRouter from 'angular-ui-router';
 
-}());
+import AppComponent from './app.component';
+
+  angular
+    .module('videoPlayerApp', [
+      ngAnimate,
+      ngAria,
+      ngMessages,
+      ngMaterial,
+      uiRouter
+    ])
+    .component('app', AppComponent);
