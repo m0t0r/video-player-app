6ee4edbffc6b708364bbbfa84a130c49ff7fda79
m0t0r <tkachenko.vitaly.job@gmail.com>
2016-02-21 18:50:11 -0500

    feat(components): Create vp-video-details components

---
+++ src/videos/components/vp-video-details/vp-video-details.component.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import template from './vp-video-details.html';
+import './vp-video-details.css';
+
+class VpVideoDetailsCtrl {
+
+}
+
+VpVideoDetailsCtrl.$inject = ['VideosService'];
+
+let VpVideoDetailsComponent = {
+  template,
+  controller: VpVideoDetailsCtrl,
+  bindings: {
+    selectedVideo: '='
+  }
+};
+
+export default VpVideoDetailsComponent;
