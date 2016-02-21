8e37d856da493f8c87fe922b8880e47186a3f8b9
m0t0r <tkachenko.vitaly.job@gmail.com>
2016-02-21 13:23:31 -0500

    chore(vp-toolbar): Add ability to add new video

--- src/components/vp-toolbar/vp-toolbar.component.js
+++ src/components/vp-toolbar/vp-toolbar.component.js
@@ -1,9 +1,39 @@
 'use strict';
 import template from './vp-toolbar.html';
+import addVideoModalTemplate from './modals/add-video-modal.html';
+import './vp-toolbar.css';
 
  class VpToolbarCtrl {
 
+   constructor($mdDialog) {
+     this.$mdDialog =$mdDialog;
+   }
+
+   addVideo($event) {
+     this.$mdDialog.show({
+       template: addVideoModalTemplate,
+       parent: angular.element(document.body),
+       targetEvent: $event,
+       controller: ['$mdDialog', function addVideoModalCtrl($mdDialog) {
+         var ctrl = this;
+
+         ctrl.cancel  = () => {
+           $mdDialog.cancel();
+         };
+
+         ctrl.save = () => {
+           $mdDialog.hide(ctrl.video);
+         };
+       }],
+       controllerAs: 'ctrl',
+       clickOutsideToClose: true
+     }).then((video) => {
+        console.log('video', video);
+     });
+   }
  }
+
+VpToolbarCtrl.$inject = ['$mdDialog'];
 
 let VpToolBarComponent = {
   template,
