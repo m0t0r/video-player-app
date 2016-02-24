49ba6ef5e9b2f02d98ac782f5de82b7f69c98e9e
Vitaly <motor.kodlife@gmail.com>
2016-02-24 14:24:24 -0500

    chore(app): Setup unit testing workflow

---
+++ spec.bundle.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import angular from 'angular';
+import mocks from 'angular-mocks';
+
+let context = require.context('./src', true, /\.spec\.js/);
+context.keys().forEach(context);
--- src/main/index.js
+++ src/main/index.js
@@ -1,12 +1,15 @@
 'use strict';
 
 import angular from 'angular';
+import ngMaterial from 'angular-material';
+import uiRouter from 'angular-ui-router';
+import common from '../common';
 
 import VpToolbarComponent from './components/vp-toolbar/vp-toolbar.component';
 import VpMenuSidenavComponent from './components/vp-menu-sidenav/vp-menu-sidenav.component';
 
 const mainModule = angular
-  .module('videoPlayerApp.main', [])
+  .module('videoPlayerApp.main', [uiRouter, ngMaterial, common])
   .component('vpToolbar', VpToolbarComponent)
   .component('vpMenuSidenav', VpMenuSidenavComponent);
 
