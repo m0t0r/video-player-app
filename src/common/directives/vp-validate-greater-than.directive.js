df7e66c36040bc80715f633af170bb5564004003
m0t0r <tkachenko.vitaly.job@gmail.com>
2016-04-07 06:49:14 -0500

    feat(directives): Create vp-validate-greater-than directive

---
+++ src/common/directives/vp-validate-greater-than.directive.js
@@ -0,0 +1,37 @@
+'use strict';
+
+function VpValidateGreaterThanLinkFn (scope, el, attrs, ngModelCtrl) {
+  function validateGreaterThan(value) {
+    var valid = (parseInt(value) > parseInt(scope.vpValidateGreaterThan));
+    ngModelCtrl.$setValidity('vpGreaterThan', valid);
+    console.log('valid', valid);
+    return valid ? value : undefined;
+  }
+
+  ngModelCtrl.$parsers.push(validateGreaterThan);
+  ngModelCtrl.$formatters.push(validateGreaterThan);
+
+  scope.$watch('vpValidateGreaterThan', function (newValue) {
+    if (ngModelCtrl.$viewValue) {
+      var parsedViewValue = ngModelCtrl.$viewValue.split(':').join('');
+      var valid = (parseInt(parsedViewValue) > parseInt(newValue));
+
+      ngModelCtrl.$setValidity('vpGreaterThan', valid);
+    }
+
+    ngModelCtrl.$setViewValue(ngModelCtrl.$viewValue);
+  });
+}
+
+let VpValidateGreaterThanDirective = () => {
+  return {
+    restrict: 'A',
+    scope: {
+      vpValidateGreaterThan: '='
+    },
+    require: 'ngModel',
+    link: VpValidateGreaterThanLinkFn
+  }
+};
+
+export default VpValidateGreaterThanDirective;
